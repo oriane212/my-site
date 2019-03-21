befb0da1c20fc06b2d1c53c58dbd2e0dc2193bd2
Oriane <oriane.daley@gmail.com>
2019-03-20 21:08:45 -0400

    create project links with styled title, description, and topics

--- createProjectLinks.js
+++ createProjectLinks.js
@@ -36,15 +36,105 @@ function getFromGH(repo_names) {
                 })
             })
             console.log(repos_filtered);
-            return repos_filtered;
+            createProjectLinks(repos_filtered);
+            //return repos_filtered;
         })
+        /*
+        .then((repos_filtered) => {
+            console.log(repos_filtered);
+            //this.createProjectLinks(repos_filtered);
+        })*/
 }
 
-let project = getFromGH(gh_repos);
+/**
+ * Insert a dash before the uppercase in camelCase str
+ * Returns either a new string with a dash before the uppercase or the original string passed
+ * Note: only works for camelCase str with a single uppercase (written for 'myReads')
+ * @param {string} str
+ */
 
-// TODO: write createProjectLinks function
+function insertDashAtUpperCase(str) {
+    let newstr = '';
+    for (let i=0; i < str.length; i++){
+        if (str.charAt(i).toUpperCase() === str.charAt(i) && str.charAt(i) !== '-') {
+            newstr = str.replace(str.charAt(i), `-${str.charAt(i)}`);
+            console.log('uppercase replaced - ' + newstr);
+        }
+    }
+    if (newstr.length > 0) {
+        return newstr;
+    } else {
+        return str;
+    }
+}
+
+/**
+ * Splits repo name on dash and rejoins with spaces
+ * Returns a new string with every dash replaced by a space
+ * @param {string} str
+ */
+function replaceDash(str) {
+    let splitOnDash = str.split('-');
+    let newstr = splitOnDash.join(' ');
+    return newstr;
+}
+
+/**
+ * Creates project links from filtered repo data
+ * Appends HTML el to DOM
+ * @param {array} repos_filtered 
+ */
+
+function createProjectLinks(repos_filtered) {
+    console.log(repos_filtered);
+    let $section_projects = document.getElementById('projects');
+    //let repos_filtered = getFromGH(repo_names);
+    repos_filtered.map((repo) => {
+        // create link el
+        let $a = document.createElement('a');
+        //$a.setAttribute('href', repo.html_url).setAttribute('id', repo.name);
+        //$a.classList.add('project');
+        // create article el
+        let $article = document.createElement('article');
+        // create text
+        let $hgroup = document.createElement('hgroup');
+        let $h2 = document.createElement('h2');
+        // create repo name
+        let newstring = replaceDash(insertDashAtUpperCase(repo.name));
+        $h2.innerText = newstring;
+
+        let $h3 = document.createElement('h3');
+        $h3.innerText = repo.description;
+
+        // topics
+        let $p = document.createElement('p');
+        $p.innerText = repo.topics.join(', ');
+        $p.classList.add('topics');
+
+        // TODO:
+        // create svg icon
+
+        // append all
+        $hgroup.append($h2, $h3);
+        $article.append($hgroup, $p);
+        $a.append($article);
+        $section_projects.append($a);
+    })
+}
+
+// TODO: finish responsive styling for projects - only basic styling has been added so far
+
+// TODO: add topics in Github to projects!
+
+// TODO: add viewport breakpoints
+    // should the createProjectLinks function just return the link El and another function handles the responsive layout and appending to the DOM?
+
+getFromGH(gh_repos);
 
 // project.description
 // project.html_url (repo page)
 // project.homepage (GitHub page), if !== ''
 // project.topics (array)
+
+
+// TODO: write createProjectIcon function
