a9c47b4c396f80e787d2c7ff62291bd527af5e42
Oriane <oriane.daley@gmail.com>
2019-08-14 21:30:53 -0400

    svg icons for all projects

--- createLines.js
+++ createLines.js
@@ -1,21 +1,37 @@
 
-const intro = document.querySelector('.intro');
+//const intro = document.querySelector('.intro');
+const main = document.querySelector('main');
 const container = document.querySelector('.container');
 const wiw = window.innerWidth;
 let lw = wiw;
 
-// create the bottom horizontal line
-let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.classList.add("h-line");
-svg.setAttribute("width", wiw);
-svg.setAttribute("height", 2);
-let bottom = document.createElementNS("http://www.w3.org/2000/svg", "line");
-bottom.setAttribute("x1", wiw);
-bottom.setAttribute("x2", 0);
-bottom.setAttribute("y1", 0);
-bottom.setAttribute("y2", 0);
-bottom.setAttribute("stroke-dasharray", lw);
-bottom.setAttribute("stroke-dashoffset", lw);
+function createHLine(id, beforebeginEl, x1, x2) {
+    // create the bottom horizontal line
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.classList.add("h-line");
+    svg.setAttribute("id", id);
+    svg.setAttribute("width", wiw);
+    svg.setAttribute("height", 2);
+    let bottom = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    bottom.setAttribute("x1", x1);
+    bottom.setAttribute("x2", x2);
+    bottom.setAttribute("y1", 0);
+    bottom.setAttribute("y2", 0);
+    bottom.setAttribute("stroke-dasharray", lw);
+    bottom.setAttribute("stroke-dashoffset", lw);
 
-svg.append(bottom);
-container.insertAdjacentElement('beforebegin', svg);
+    svg.append(bottom);
+    beforebeginEl.insertAdjacentElement('beforebegin', svg);
+}
+
+// header line 1
+createHLine("headerline1", main, 0, wiw);
+
+// header line 2
+createHLine("headerline2", container, wiw, 0);
+
+
+///////// TODO: ////////
+
+// fix position for #headerline1 responsively
+// add delay for its animation
--- createProjectLinks.js
+++ createProjectLinks.js
@@ -1,7 +1,167 @@
 
+let ox = 0;
+const rectObjs = [
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'chart-alt',
+        title: 'Chart Alt',
+        caption: '',
+        tools: '',
+        link: '',
+        icon: [
+            { el: 'rect', attributes: [['x', 15 + ox], ['y', 40], ['rx', "2"], ['ry', "2"], ['width', '8'], ['height', '15']] },
+            { el: 'rect', attributes: [['x', 27 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '8'], ['height', '25']] },
+            { el: 'rect', attributes: [['x', 39 + ox], ['y', 15], ['rx', "2"], ['ry', "2"], ['width', '8'], ['height', '40']] },
+            { el: 'rect', attributes: [['x', 40 + ox], ['y', 60], ['rx', "2"], ['ry', "2"], ['width', '40'], ['height', '6']] },
+            { el: 'rect', attributes: [['x', 40 + ox], ['y', 70], ['rx', "2"], ['ry', "2"], ['width', '40'], ['height', '6']] },
+            { el: 'rect', attributes: [['x', 40 + ox], ['y', 80], ['rx', "2"], ['ry', "2"], ['width', '40'], ['height', '6']] }
+        ]
+    },
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'coffee-map',
+        title: 'Coffee Map',
+        caption: 'Map of coffee places',
+        tools: 'ReactJS, Mapbox GL JS',
+        link: 'https://github.com/oriane212/coffee-map',
+        icon: [
+            { el: 'circle', attributes: [['cx', ox + 50], ['cy', 40], ['r', '4']] },
+            { el: 'polygon', attributes: [['points', `${ox + 46},${40} ${ox + 54},${40}, ${ox + 50.5},${55} ${ox + 49.5},${55}`]] },
+            { el: 'circle', attributes: [['cx', ox + 60], ['cy', 75], ['r', '4']] },
+            { el: 'polygon', attributes: [['points', `${ox + 56},${75} ${ox + 64},${75}, ${ox + 60.5},${90} ${ox + 59.5},${90}`]] },
+            { el: 'circle', attributes: [['cx', ox + 80], ['cy', 25], ['r', '4']] },
+            { el: 'polygon', attributes: [['points', `${ox + 76},${25} ${ox + 84},${25}, ${ox + 80.5},${40} ${ox + 79.5},${40}`]] }
+        ]
+    },
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'myReads',
+        title: 'My Reads',
+        caption: 'Bookshelves app',
+        tools: 'ReactJS, React-Bootstrap',
+        link: 'https://github.com/oriane212/myReads',
+        icon: [
+            { el: 'rect', attributes: [['x', 10 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '25']] },
+            { el: 'rect', attributes: [['x', 30 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '25']] },
+            { el: 'rect', attributes: [['x', 50 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '25']] }
+        ]
+    },
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'frogger',
+        title: 'Frogger Game',
+        caption: 'Star Wars themed game',
+        tools: 'HTML, CSS, JS',
+        link: 'https://github.com/oriane212/frogger',
+        icon: [
+            { el: 'circle', attributes: [['cx', 50 + ox], ['cy', -10], ['r', '30']] },
+            { el: 'circle', attributes: [['cx', 50 + ox], ['cy', 85], ['r', '5']] }
+        ]
+    },
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'personalized-language-learning',
+        title: 'Personalized Language Learning',
+        caption: 'UX design for mobile app',
+        tools: 'Balsamiq, Sketch, Flinto',
+        link: 'https://github.com/oriane212/personalized-language-learning',
+        icon: [
+            { el: 'rect', attributes: [['x', 40 + ox], ['y', 40], ['rx', "2"], ['ry', "2"], ['width', '5'], ['height', '20']] },
+            { el: 'rect', attributes: [['x', 50 + ox], ['y', 40], ['rx', "2"], ['ry', "2"], ['width', '10'], ['height', '20']] },
+            { el: 'rect', attributes: [['x', 65 + ox], ['y', 40], ['rx', "2"], ['ry', "2"], ['width', '20'], ['height', '20']] }
+        ]
+    },
+    {
+        x: ox,
+        y: 0,
+        width: 100,
+        height: 100,
+        type: 'project',
+        id: 'memory-game',
+        title: 'Memory Game',
+        caption: 'Web game of matching cards',
+        tools: 'HTML, CSS, JS',
+        link: 'https://github.com/oriane212/memory-game',
+        icon: [
+            { el: 'rect', attributes: [['x', 33 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '15']] },
+            { el: 'rect', attributes: [['x', 33 + ox], ['y', 50], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '15']] },
+            { el: 'rect', attributes: [['x', 53 + ox], ['y', 30], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '15']] },
+            { el: 'rect', attributes: [['x', 53 + ox], ['y', 50], ['rx', "2"], ['ry', "2"], ['width', '15'], ['height', '15']] }
+        ]
+    }
+]
+
+function createSVGicon(repo_name) {
+
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.classList.add('projecticon');
+    svg.setAttribute('id', repo_name + `_icon`);
+    svg.setAttribute('x', 0);
+    svg.setAttribute('y', 0);
+    svg.setAttribute('width', 100);
+    svg.setAttribute('height', 100);
+
+    let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute('fill', 'rgba(0,0,0,0.1)');
+    rect.setAttribute('x', 0);
+    rect.setAttribute('y', 0);
+    rect.setAttribute('width', 100);
+    rect.setAttribute('height', 100);
+
+    svg.appendChild(rect);
+
+    rectObjs.forEach((repo) => {
+        if (repo.id === repo_name) {
+
+            // create unique svg icon elements
+            let svgGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+            svgGroup.setAttribute('role', 'img');
+            svgGroup.setAttribute('fill', 'white');
+
+            for (let icon of repo.icon) {
+                let iconEl = document.createElementNS("http://www.w3.org/2000/svg", icon.el);
+                for (let attribute of icon.attributes) {
+                    iconEl.setAttribute(attribute[0], attribute[1]);
+                }
+                console.log(iconEl);
+                svgGroup.appendChild(iconEl);
+            }
+
+            svg.appendChild(svgGroup);
+
+        }
+
+        return svg;
+    })
+
+    return svg;
+
+}
+
+
 // list of Github repo names to create project links from
 const gh_repos = [
-    'chart-alt', 'coffee-map', 'myReads', 'restaurant-reviews', 'frogger', 'memory-game', 'personalized-language-learning'
+    'chart-alt', 'coffee-map', 'myReads', 'frogger', 'memory-game', 'personalized-language-learning'
 ]
 
 /**
@@ -39,11 +199,11 @@ function getFromGH(repo_names) {
             createProjectLinks(repos_filtered);
             //return repos_filtered;
         })
-        /*
-        .then((repos_filtered) => {
-            console.log(repos_filtered);
-            //this.createProjectLinks(repos_filtered);
-        })*/
+    /*
+    .then((repos_filtered) => {
+        console.log(repos_filtered);
+        //this.createProjectLinks(repos_filtered);
+    })*/
 }
 
 /**
@@ -55,7 +215,7 @@ function getFromGH(repo_names) {
 
 function insertDashAtUpperCase(str) {
     let newstr = '';
-    for (let i=0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
         if (str.charAt(i).toUpperCase() === str.charAt(i) && str.charAt(i) !== '-') {
             newstr = str.replace(str.charAt(i), `-${str.charAt(i)}`);
             console.log('uppercase replaced - ' + newstr);
@@ -95,7 +255,16 @@ function createProjectLinks(repos_filtered) {
         let $a = document.createElement('a');
         $a.setAttribute('href', repo.html_url);
         $a.setAttribute('id', repo.name);
+        $a.setAttribute('target', '_blank');
         //$a.classList.add('project');
+
+
+        // create project icon
+        let $div_icon = document.createElement('div');
+        $div_icon.classList.add('icon');
+        let $svg_icon = createSVGicon(repo.name);
+        $div_icon.appendChild($svg_icon);
+
         // create article el
         let $article = document.createElement('article');
         // create text
@@ -113,18 +282,27 @@ function createProjectLinks(repos_filtered) {
         $p.innerText = repo.topics.join(', ');
         $p.classList.add('topics');
 
-        // TODO:
-        // create svg icon
-
         // append all
         $hgroup.append($h2, $h3);
         $article.append($hgroup, $p);
-        $a.append($article);
+        $a.append($div_icon, $article);
 
         //project_links.push($a);
 
         $div_projects.append($a);
     })
+
+
+    /*
+    let coffeeIcon = createSVGicon('coffee-map');
+    console.log(coffeeIcon);
+
+    let coffee = document.querySelector("#coffee-map .icon");
+    console.log(coffee);
+    coffee.appendChild(coffeeIcon);
+    */
+
+    //coffee.insertAdjacentElement('afterbegin', coffeeIcon);
 
     //return project_links;
 }
@@ -136,7 +314,7 @@ function createProjectLinks(repos_filtered) {
 // TODO: add topics in Github to projects!
 
 // TODO: add viewport breakpoints
-    // should the createProjectLinks function just return the link El and another function handles the responsive layout and appending to the DOM?
+// should the createProjectLinks function just return the link El and another function handles the responsive layout and appending to the DOM?
 
 getFromGH(gh_repos);
 
